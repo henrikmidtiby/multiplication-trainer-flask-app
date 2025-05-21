49afc9530cde9e8d93799b8e00a67c665120008b
Henrik Skov Midtiby <hemi@mmmi.sdu.dk>
2025-05-21 08:36:50 +0200

    Added requirements.

--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+from icecream import ic
 from flask import (
     current_app,
     render_template,
@@ -11,3 +12,9 @@ app = current_app
 @app.route("/")
 def index():
     return render_template("multiplication_trainer.html")
+
+@app.route("logger/v1/<int:n_exercises>/<float:used_time>/<string:group>")
+def log_used_time(n_exercises, used_time, group):
+    ic(n_exercises)
+    ic(used_time)
+    ic(group)
